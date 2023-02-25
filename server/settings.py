e060fe89832a52881457aeb21261c3a0977f3c12
Dhananjayan P N <pndjay@gmail.com>
2023-02-25 17:27:05 -0600

    setup docker

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fe$f8us8q&fou12on0scsfv*v5kohv%b69)^lc-&b365d@qvq('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
