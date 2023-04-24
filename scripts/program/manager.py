8bf6c2cbb7a34ce31fb2c70d4db0aa00b26721ed
yan <zhyan0096@gmail.com>
2023-04-23 23:28:45 -0500

    manager doc

---
+++ scripts/program/manager.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Manager module docstring
+
+1. Manager module should be able to create folder to hold projects' metadata and associated tasks 
+2. Manager module should be able to create folder to hold tasks' metadata and all related data file
+3. Manager module should be able to create metadata file for projects and tasks
+4. Manager module should be able to update metadata file when user create or delete project or task
+"""
+
+def create_project_folder(path):
+    """
+    create_project_folder needs the project's information to generate a unique folder
+    what information is required needs discussion
+    """
+    pass
+
+def create_task_folder(path):
+    """
+    create_task_folder needs the task's information to generate a unique folder
+    what information is required needs discussion
+    """
+    pass
+
+def create_metadata(info):
+    """
+    An ideal create_metadata should be able to create either project metadata or task metadata
+    what infomation should be stored in metadata needs discussion
+    """
+    pass
+
+def update_metadata(path):
+    """
+    update_metadata should be able to update the latest information of tasks or projects on server
+    """
