74ca0c5c112c3282961cf1e163cb350befa6e4bf
littlekimber <zhyan0096@gmail.com>
2023-05-18 15:44:21 +0000

    fix scripts module not found error

--- scripts/tests/test.sh
+++ scripts/tests/test.sh
@@ -2,6 +2,7 @@
 if [ -d "TestData" ]
 then
     echo "Test data exists, run unit tests"
+    python3 
 else
     echo "Test data not exists, download begins"
     wget https://cemrcstatic.blob.core.windows.net/cryoet-tomoflows/TestData.tar
