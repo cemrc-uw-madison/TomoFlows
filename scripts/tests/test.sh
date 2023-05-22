57b7b03c13019d8b3fd93ee4f8fb4f3321fd4761
littlekimber <zhyan0096@gmail.com>
2023-05-22 04:02:12 +0000

    finish test shell script for task gain

--- scripts/tests/test.sh
+++ scripts/tests/test.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-if [ -d "TestData" ]
-then
-    echo "Test data exists, run unit tests"
-    python3 
+if [ "$1" == "update" ] 
+then 
+    if [ -d "TestData"]
+    then 
+        echo "Deleting old data..."
+        rm -r TestData
+        echo "Downloading latest data..."    
+        wget https://cemrcstatic.blob.core.windows.net/cryoet-tomoflows/TestData.tar
+        tar -xvf TestData.tar
+        rm TestData.tar
+        echo "Test data exists, run unit tests..."
+        pytest test_task_gain.py
+    fi
 else
-    echo "Test data not exists, download begins"
-    wget https://cemrcstatic.blob.core.windows.net/cryoet-tomoflows/TestData.tar
-    tar -xvf TestData.tar
-    rm TestData.tar
+    if [ -d "TestData" ]
+    then
+        echo "Test data exists, run unit tests..."
+        pytest test_task_gain.py 
+    else
+        echo "Test data not exists, download begins..."
+        wget https://cemrcstatic.blob.core.windows.net/cryoet-tomoflows/TestData.tar
+        tar -xvf TestData.tar
+        rm TestData.tar
+        echo "Test data exists, run unit tests..."
+        pytest test_task_gain.py
+    fi
 fi
